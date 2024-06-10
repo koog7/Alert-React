19f40f65389e27bdd58964a2f01f30d3ed2c1c6c
koog7 <k.ognev709@mail.com>
2024-06-10 19:45:39 +0600

    Измений тип данных которые передаются в onDismiss c boolean на func. Подправлен код под данные изменения

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,18 @@ const App = () => {
     const makeFalse = () => {
       setModalShow(false)
     }
+
+    const closeAlert = () => {
+        console.log('Alert dismissed');
+    };
+
     return (
         <>
             <button className={'w-25 btn btn-primary'} onClick={() => setModalShow(true)}>Модальное окно</button>
             <Modal onClose={makeFalse} title={'Title'} show={ModalShow}><p>This is modal content</p></Modal>
             <Alert type="success">This is a success type alert</Alert>
-            <Alert type="warning" onDismiss={true}>This is a warning type alert</Alert>
-            <Alert type="info" onDismiss={true}>This is a info type alert</Alert>
+            <Alert type="warning" onDismiss={closeAlert}>This is a warning type alert</Alert>
+            <Alert type="info" onDismiss={closeAlert}>This is a info type alert</Alert>
             <Alert type="dark">This is a dark type alert</Alert>
         </>
     )
--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,14 +1,17 @@
 import React, {useState} from "react";
 interface Props extends React.PropsWithChildren{
     type:string;
-    onDismiss?: boolean;
+    onDismiss?: () => void;
 }
 
 const Alert: React.FC<Props> = ({type , onDismiss,children}) => {
     const [display , setDisplay ] = useState(true)
 
     const makeFalse = () => {
-        setDisplay(false)
+        setDisplay(false);
+        if (onDismiss) {
+            onDismiss();
+        }
     }
 
     return (
